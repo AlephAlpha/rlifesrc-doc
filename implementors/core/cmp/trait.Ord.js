d6a5b0b685aaa592c119a88de86811d231eb19e2
AlephAlpha <alephalpha911@gmail.com>
2021-01-31 10:02:08 +0000

    Update from https://github.com/AlephAlpha/rlifesrc/commit/5b7c34f32124ed5d3f0bb2aea856945f316c95a6

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -6,7 +6,7 @@ implementors["futures_util"] = [{"text":"impl&lt;T:&nbsp;Ord&gt; Ord for AllowSt
 implementors["log"] = [{"text":"impl&lt;'k&gt; Ord for Key&lt;'k&gt;","synthetic":false,"types":[]},{"text":"impl Ord for Level","synthetic":false,"types":[]},{"text":"impl Ord for LevelFilter","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Ord for Metadata&lt;'a&gt;","synthetic":false,"types":[]},{"text":"impl&lt;'a&gt; Ord for MetadataBuilder&lt;'a&gt;","synthetic":false,"types":[]}];
 implementors["mio"] = [{"text":"impl Ord for Interest","synthetic":false,"types":[]},{"text":"impl Ord for Token","synthetic":false,"types":[]}];
 implementors["proc_macro2"] = [{"text":"impl Ord for Ident","synthetic":false,"types":[]}];
-implementors["rlifesrc_lib"] = [{"text":"impl Ord for SkipLevel","synthetic":false,"types":[]},{"text":"impl Ord for ImplFlags","synthetic":false,"types":[]},{"text":"impl Ord for ImplFlags","synthetic":false,"types":[]}];
+implementors["rlifesrc_lib"] = [{"text":"impl Ord for ImplFlags","synthetic":false,"types":[]},{"text":"impl Ord for ImplFlags","synthetic":false,"types":[]}];
 implementors["signal_hook_registry"] = [{"text":"impl Ord for SigId","synthetic":false,"types":[]}];
 implementors["smallvec"] = [{"text":"impl&lt;A:&nbsp;Array&gt; Ord for SmallVec&lt;A&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;A::Item: Ord,&nbsp;</span>","synthetic":false,"types":[]}];
 implementors["syn"] = [{"text":"impl Ord for Lifetime","synthetic":false,"types":[]}];
--- rlifesrc_lib/config/sidebar-items.js
+++ rlifesrc_lib/config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["NewState","How to choose a state for an unknown cell."],["SkipLevel","What patterns are considered boring and should be skip."]],"mod":[["d8","Configurations related to the dihedral group D8."],["search_order",""]],"struct":[["Config","World configuration."]]});
+initSidebarItems({"enum":[["NewState","How to choose a state for an unknown cell."]],"mod":[["d8","Configurations related to the dihedral group D8."],["search_order",""]],"struct":[["Config","World configuration."]]});
