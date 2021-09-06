27a2e5aa8e7f9fc2569b4a6719ab4aabfb0bf1c2
AlephAlpha <alephalpha911@gmail.com>
2021-09-06 02:46:28 +0000

    :rocket: Update from https://github.com/AlephAlpha/rlifesrc/commit/7347f8b4d718ead578e02f356090bcf237c89b89

--- signal_hook_mio/sidebar-items.js
+++ signal_hook_mio/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["implement_signals_with_pipe",""]],"mod":[["v0_7","A module for integrating signal handling with the MIO 0.7 runtime."]]});
+initSidebarItems({"mod":[["v0_7","A module for integrating signal handling with the MIO 0.7 runtime."]]});
