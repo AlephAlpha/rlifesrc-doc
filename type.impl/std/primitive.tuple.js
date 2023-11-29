a42841de9e4829054343b465d7549a4bbba0bdd7
AlephAlpha <alephalpha911@gmail.com>
2023-11-29 02:17:13 +0000

    :rocket: Update from https://github.com/AlephAlpha/rlifesrc/commit/8a710500e9c254b3851deda1c7b86a1468d06365

---
+++ type.impl/std/primitive.tuple.js
@@ -0,0 +1,4 @@
+(function() {var type_impls = {
+"ca_formats":[],
+"rlifesrc_lib":[]
+};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
